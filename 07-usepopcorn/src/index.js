5ec8e82eaa0c84c67c40fc89aa7b6ac30cd3be91
denev288 <denev288@gmail.com>
2025-10-20 13:50:01 +0300

    07 and 08 upload

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import './index.css';
+// import App from './App';
+import StarRating from "./StarRating";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "okay", "Good", "Amazing"]}
+      size = {48}
+    />
+    <StarRating maxRating={5} size={24} color="red" defaultRating={3}/>
   </React.StrictMode>
 );
-
