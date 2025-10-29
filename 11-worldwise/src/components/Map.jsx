c90cb92b2f41beecb421ceb74d07f5e9cc7d2e6d
denev288 <denev288@gmail.com>
2025-10-29 09:26:53 +0200

    nested routes and outlet

---
+++ 11-worldwise/src/components/Map.jsx
@@ -0,0 +1,12 @@
+import styles from './Map.module.css'
+
+
+function Map() {
+    return (
+        <div className={styles.mapContainer}>
+            Mappricing
+        </div>
+    )
+}
+
+export default Map
