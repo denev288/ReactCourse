c8b5909ac1a6d03216879474ce4dde91c4e4cde5
denev288 <denev288@gmail.com>
2025-11-12 10:42:54 +0200

    tailwind styling

--- 16-fast-react-pizza/src/ui/AppLayout.jsx
+++ 16-fast-react-pizza/src/ui/AppLayout.jsx
@@ -4,17 +4,19 @@ import Header from "./Header";
 import Loader from "./Loader";
 
 function AppLayout() {
-  const navigation = useNavigation()
-  const isLoading = navigation.state === "loading"
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
-      {isLoading && <Loader/>}
-      <Header />
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
 
-      <main>
-        <Outlet />
-      </main>
+      <Header />
+      <div className="">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
