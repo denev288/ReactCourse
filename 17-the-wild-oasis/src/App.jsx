21ef6d4daa890b7145e2b8ec3fb4bdf34daa3a2b
denev288 <denev288@gmail.com>
2025-11-19 12:23:06 +0200

    supabase and reactQuery

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -9,14 +9,26 @@ import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import GlobabStyles from "./styles/GlobaStyles";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster} from "react-hot-toast"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false}/>
       <GlobabStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout/>}>
+          <Route element={<AppLayout />}>
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
@@ -29,7 +41,24 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+      <Toaster position="top-center" gutter={12} containerStyle={{margin: "8px"}}
+      toastOptions={{
+        success:{
+          duration: 3000,
+        },
+        error: {
+          duration: 5000
+        },
+        style: {
+          fontSize: '16px',
+          maxWidth: '500px',
+          padding: '16px 24px',
+          backgroundColor: 'var(--color-grey-0)',
+          color: 'var(--color-grey-700)',
+
+        }
+      }}/>
+    </QueryClientProvider>
   );
 }
 
