16d4d75231c6a2ba783ca7fb02acf3a051551420
denev288 <denev288@gmail.com>
2025-12-08 10:55:05 +0200

    updating data and password

--- 17-the-wild-oasis/src/features/authentication/UpdatePasswordForm.jsx
+++ 17-the-wild-oasis/src/features/authentication/UpdatePasswordForm.jsx
@@ -1,15 +1,15 @@
 import { useForm } from 'react-hook-form';
-import Button from 'ui/Button';
-import Form from 'ui/Form';
-import FormRow from 'ui/FormRow';
-import Input from 'ui/Input';
+import Button from "../../ui/Button";
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
 import { useUpdateUser } from './useUpdateUser';
 
 function UpdatePasswordForm() {
   const { register, handleSubmit, formState, getValues, reset } = useForm();
   const { errors } = formState;
 
-  const { mutate: updateUser, isLoading: isUpdating } = useUpdateUser();
+  const { updateUser, isLoading: isUpdating } = useUpdateUser();
 
   function onSubmit({ password }) {
     updateUser({ password }, { onSuccess: () => reset() });
